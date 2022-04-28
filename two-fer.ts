b8d11dc8c612f2daf8ec34473fa0b7eb3901dc7a
Blair Bristow <blairhbristow@gmail.com>
2022-04-28 12:48:25 -0400

    Add solution

--- two-fer.ts
+++ two-fer.ts
@@ -1,1 +1,7 @@
-export function twoFer() {}
+export function twoFer(name: string) {
+    if(name == undefined) {
+        return `One for you, one for me.`
+    } else {
+        return `One for ${name}, one for me.`
+    }
+}
